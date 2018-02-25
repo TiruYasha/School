34710017490ff1ac6cccac05029502ede1923a42
renebuunk <renebuunk1996@gmail.com>
2018-02-25 13:21:54 +0100

    Mergesort recursive

---
+++ SortSorting/src/main/java/Sort/Sorting/MergeSort.java
@@ -0,0 +1,63 @@
+package Sort.Sorting;
+
+public class MergeSort {
+
+    public static int[] sort(int[] arrayToSort) {
+        int[] tmpArray = new int[arrayToSort.length];
+        sort(arrayToSort, tmpArray, 0, arrayToSort.length - 1);
+
+        return arrayToSort;
+    }
+
+    private static void sort(int[] arrayToSort, int[] tmpArray, int left, int right) {
+        if (left < right) {
+            int center = (left + right) / 2;
+            sort(arrayToSort, tmpArray, left, center);
+            sort(arrayToSort, tmpArray, center + 1, right);
+            merge(arrayToSort, tmpArray, left, center + 1, right);
+        }
+    }
+
+    private static void merge(int[] arrayToSort, int[] tmpArray, int left, int rightPos, int rightEnd) {
+        merge(arrayToSort, tmpArray, left, rightPos - 1, rightEnd, left, rightPos, left);
+    }
+
+    private static void merge(int[] arrayToSort,
+                              int[] tmpArray,
+                              int left,
+                              int leftEnd,
+                              int rightEnd,
+                              int leftIndex,
+                              int rightIndex,
+                              int tmpArrayIndex) {
+
+        if(tmpArrayIndex> rightEnd){
+            tmpArrayToSortedArray(arrayToSort, tmpArray, left, rightEnd);
+
+            return;
+        }
+
+        if (leftIndex > leftEnd) {
+            tmpArray[tmpArrayIndex] = arrayToSort[rightIndex];
+            merge(arrayToSort, tmpArray, left, leftEnd, rightEnd, leftIndex, rightIndex + 1, tmpArrayIndex + 1);
+        }
+        else if (rightIndex > rightEnd) {
+            tmpArray[tmpArrayIndex] = arrayToSort[leftIndex];
+            merge(arrayToSort, tmpArray, left, leftEnd, rightEnd, leftIndex + 1, rightIndex, tmpArrayIndex + 1);
+        }
+        else if (arrayToSort[leftIndex] < arrayToSort[rightIndex]) {
+            tmpArray[tmpArrayIndex] = arrayToSort[leftIndex];
+            merge(arrayToSort, tmpArray, left, leftEnd, rightEnd, leftIndex + 1, rightIndex, tmpArrayIndex + 1);
+        }
+        else {
+            tmpArray[tmpArrayIndex] = arrayToSort[rightIndex];
+            merge(arrayToSort, tmpArray, left, leftEnd, rightEnd, leftIndex, rightIndex + 1, tmpArrayIndex + 1);
+        }
+    }
+
+    private static void tmpArrayToSortedArray(int[] arrayToSort, int[] tmpArray, int left, int end) {
+        for (int i = left; i < end + 1; i++) {
+            arrayToSort[i] = tmpArray[i];
+        }
+    }
+}
---
+++ SortSorting/src/test/java/Sort/Sorting/MergeSortTest.java
@@ -0,0 +1,27 @@
+package Sort.Sorting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class MergeSortTest {
+
+//    @Test
+//    public void mergeSortReturnsArray() {
+//        int[] arrayToSort = {1};
+//
+//        int[] result = MergeSort.sort(arrayToSort);
+//
+//        assertArrayEquals(arrayToSort, result);
+//    }
+
+    @Test
+    public void mergeSortReturnsSortedArray() {
+        int[] arrayToSort = {4, 3, 7, 3, 2, 1, 3, 9, 7, 5};
+        int[] sortedArray = {1, 2, 3, 3, 3, 4, 5, 7, 7, 9};
+
+        int[] result = MergeSort.sort(arrayToSort);
+
+        assertArrayEquals(sortedArray, result);
+    }
+}
