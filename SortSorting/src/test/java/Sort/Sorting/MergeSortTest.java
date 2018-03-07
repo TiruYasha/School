8c94863f797a76868787c1a0073167c2b5bd8697
renebuunk <renebuunk1996@gmail.com>
2018-03-07 18:48:03 +0100

    Mergesort non recursive

--- SortSorting/src/test/java/Sort/Sorting/MergeSortTest.java
+++ SortSorting/src/test/java/Sort/Sorting/MergeSortTest.java
@@ -2,6 +2,8 @@ package Sort.Sorting;
 
 import org.junit.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.assertArrayEquals;
 
 public class MergeSortTest {
@@ -10,7 +12,7 @@ public class MergeSortTest {
     public void mergeSortReturnsArray() {
         int[] arrayToSort = {1};
 
-        int[] result = MergeSort.sort(arrayToSort);
+        int[] result = MergeSort.sortRecursive(arrayToSort);
 
         assertArrayEquals(arrayToSort, result);
     }
@@ -20,7 +22,17 @@ public class MergeSortTest {
         int[] arrayToSort = {4, 3, 7, 3, 2, 1, 3, 9, 7, 5};
         int[] sortedArray = {1, 2, 3, 3, 3, 4, 5, 7, 7, 9};
 
-        int[] result = MergeSort.sort(arrayToSort);
+        int[] result = MergeSort.sortRecursive(arrayToSort);
+
+        assertArrayEquals(sortedArray, result);
+    }
+
+    @Test
+    public void mergeSortNonRecursive(){
+        int[] arrayToSort = {4, 3, 7, 3, 2, 1, 3, 9, 7, 5};
+        Integer[] sortedArray = {1, 2, 3, 3, 3, 4, 5, 7, 7, 9};
+
+        Integer[] result =  MergeSort.mergeSort(IntStream.of(arrayToSort).boxed().toArray(Integer[]::new));
 
         assertArrayEquals(sortedArray, result);
     }
