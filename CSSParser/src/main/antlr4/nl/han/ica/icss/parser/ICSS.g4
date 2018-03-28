2d70cb3c282d1f5e14e84f765ccf933219feca36
renebuunk <renebuunk1996@gmail.com>
2018-03-28 22:21:27 +0200

    Level 0 icss is being parsed

--- CSSParser/src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4
+++ CSSParser/src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4
@@ -1,10 +1,10 @@
 grammar ICSS;
 
-stylesheet: stylesheetsection*;
+stylesheet: stylesheetsection* EOF;
 
 stylesheetsection: selectorStyle;
 
-selectorStyle: SELECTOR SELECTOR_OPEN declaration* SELECTOR_CLOSE;
+selectorStyle: selector SELECTOR_OPEN declaration* SELECTOR_CLOSE;
 
 // Maak een reference voor bv de constname om die op te zoeken in de boom
 // constantReference: CONSTNAME
@@ -13,23 +13,38 @@ selectorStyle: SELECTOR SELECTOR_OPEN declaration* SELECTOR_CLOSE;
 // Expression SUMOP expression #addExpression
 // SUMOP: '+';
 // Addoperation knoop aanmaken die 2 kinderen heeft dus die ook maar weer ns op de stack zetten
-declaration: DECLARATION_STRING COLON;
+selector: DECLARATION_STRING #TagSelector|
+          CLASSSELECTOR #ClassSelector |
+          IDSELECTOR #IdSelector;
 
-WHITESPACE: [ \t\r\n]+ -> skip;
-INT: [0-9]+;
+declaration: declarationProperty COLON expression SEMICOLON;
 
-DECLARATION_STRING: [a-zA-Z-]+;
-PXVALUE: [0-9]+'px';
+declarationProperty: DECLARATION_STRING;
 
-SELECTOR: [a-zA-Z0-9]+;
+expression: PIXEL_LITERAL #PixelLiteral |
+    PERCENTAGE_LITERAL #PercentageLiteral |
+    COLOR_LITERAL #ColorLiteral;
+
+COLOR_LITERAL: '#'[a-f0-9]+;
+PIXEL_LITERAL: [0-9]+'px';
+PERCENTAGE_LITERAL: [0-9]+'%';
+
+CLASSSELECTOR: '.'STRING;
+IDSELECTOR: '#'STRING;
 SELECTOR_OPEN: '{';
 SELECTOR_CLOSE: '}';
 
+DECLARATION_STRING: DECLARATION;
+
+fragment STRING: [a-zA-Z]+;
+fragment DECLARATION: [a-zA-Z-]+;
 
 COLON: ':';
 SEMICOLON: ';';
 HASHTAG: '#';
 DOT: '.';
+
+WHITESPACE: [ \t\r\n]+ -> skip;
 
 
 
--- CSSParser/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ CSSParser/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -8,36 +8,105 @@ import nl.han.ica.icss.ast.*;
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
  */
 public class ASTListener extends ICSSBaseListener {
-	
-	//Accumulator attributes:
-	private AST ast;
-	private Stack<ASTNode> currentContainer; //This is a hint...
 
-	public ASTListener() {
-		ast = new AST();
-		currentContainer = new Stack<>();
+    //Accumulator attributes:
+    private AST ast;
+    private Stack<ASTNode> currentContainer; //This is a hint...
 
-		//ast.root.addChild(new ConstantDefinition());
+    public ASTListener() {
+        ast = new AST();
+        currentContainer = new Stack<>();
+
+        //ast.root.addChild(new ConstantDefinition());
 
 
+    }
 
-	}
+    //Constantdefinition wordt gegenereerd
+    // Maak een enterconstantreference om die knoop value eruit te halen
+    //	public void EnterConstDefinition(ICSSParser.ConstantDefinition ctx){
+    //	    ConstantDefinition def = new ConstantDefinition();
+    //
+    //	    //def.name = new ConstantReference(ctx.getChild(1).getText());
+    //	    def.expression = new PixelLiteral("10px");
+    //
+    //	    //push de constantdefinition
+    //	    currentContainer.push(def);
+    //
+    //	    //Dan kun je in de constantreference peek voor de value
+    //
+    //	    ast.root.addChild(def);
+    //    }
 
-	//Constantdefinition wordt gegenereerd
-	// Maak een enterconstantreference om die knoop value eruit te halen
-//	public void EnterConstDefinition(ICSSParser.ConstantDefinition ctx){
-//	    ConstantDefinition def = new ConstantDefinition();
-//
-//	    //def.name = new ConstantReference(ctx.getChild(1).getText());
-//	    def.expression = new PixelLiteral("10px");
-//
-//	    //push de constantdefinition
-//	    currentContainer.push(def);
-//
-//	    //Dan kun je in de constantreference peek voor de value
-//
-//	    ast.root.addChild(def);
-//    }
+
+    @Override
+    public void enterSelectorStyle(ICSSParser.SelectorStyleContext ctx) {
+        Stylerule rule = new Stylerule();
+
+        currentContainer.push(rule);
+    }
+
+    @Override
+    public void exitSelectorStyle(ICSSParser.SelectorStyleContext ctx) {
+        ASTNode rule = currentContainer.pop();
+
+        ast.root.addChild(rule);
+    }
+
+    @Override
+    public void enterTagSelector(ICSSParser.TagSelectorContext ctx) {
+        TagSelector selector = new TagSelector(ctx.getText());
+        currentContainer.peek().addChild(selector);
+    }
+
+    @Override
+    public void enterClassSelector(ICSSParser.ClassSelectorContext ctx) {
+        ClassSelector selector = new ClassSelector(ctx.getText());
+        currentContainer.peek().addChild(selector);
+    }
+
+    @Override
+    public void enterIdSelector(ICSSParser.IdSelectorContext ctx) {
+        IdSelector selector = new IdSelector(ctx.getText());
+        currentContainer.peek().addChild(selector);
+    }
+
+    @Override
+    public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
+        Declaration declaration = new Declaration();
+        currentContainer.push(declaration);
+    }
+
+    @Override
+    public void exitDeclaration(ICSSParser.DeclarationContext ctx) {
+        ASTNode declaration = currentContainer.pop();
+
+        currentContainer.peek().addChild(declaration);
+    }
+
+    @Override
+    public void enterDeclarationProperty(ICSSParser.DeclarationPropertyContext ctx){
+        Declaration declaration = (Declaration) currentContainer.peek();
+        declaration.property = ctx.getText();
+    }
+
+    @Override
+    public void enterPixelLiteral(ICSSParser.PixelLiteralContext ctx){
+        Declaration declaration = (Declaration) currentContainer.peek();
+        declaration.expression = new PixelLiteral(ctx.getText());
+    }
+
+    @Override
+    public void enterPercentageLiteral(ICSSParser.PercentageLiteralContext ctx){
+        Declaration declaration = (Declaration) currentContainer.peek();
+        declaration.expression = new PercentageLiteral(ctx.getText());
+    }
+
+    @Override
+    public void enterColorLiteral(ICSSParser.ColorLiteralContext ctx){
+        Declaration declaration = (Declaration) currentContainer.peek();
+        declaration.expression = new ColorLiteral(ctx.getText());
+    }
 
     public AST getAST() {
         return ast;
